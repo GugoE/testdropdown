8b04bb1a13eb403870d7b87412837aad6558c2e7
Gurgen <gaegiazaryan@gmail.com>
2021-07-21 18:44:43 +0400

    fix

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:GugoE/GugoE.github.io.git master:gh-pages
+git push -f git@github.com:GugoE/testdropdown master:gh-pages
 cd -
